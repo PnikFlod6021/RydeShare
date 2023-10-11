022e22bdf27b5a32097e75d5354b7ecf186664b5
PnikFlod6021 <sri062106@outlook.com>
2023-10-11 00:28:22 -0500

    App

---
+++ app/routes.py
@@ -0,0 +1,13 @@
+from flask import Flask, request, render_template, redirect, url_for
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('homepage.html')
+
+@app.route('/register')
+def register():
+    return render_template('register.html')
+if __name__ == '__main__':
+    app.run(debug=True)
