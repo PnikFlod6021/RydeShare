d07dd3a6714113d437f819f9f8a7c9df9e0a8dda
PnikFlod6021 <sri062106@outlook.com>
2023-10-15 19:43:15 -0500

    Functional Login and Sign In Page

--- app/routes.py
+++ app/routes.py
@@ -1,13 +1,54 @@
-from flask import Flask, request, render_template, redirect, url_for
+from flask import Flask, render_template, request, redirect, url_for, flash
+import sqlite3
 
 app = Flask(__name__)
+app.config['STATIC_FOLDER'] = 'static'
+
+
+def init_db():
+    conn = sqlite3.connect('database.db')
+    cursor = conn.cursor()
+    cursor.execute('CREATE TABLE IF NOT EXISTS users (id INTEGER PRIMARY KEY, username TEXT, password TEXT)')
+    conn.commit()
+    conn.close()
+
+init_db()
 
 @app.route('/')
 def home():
-    return render_template('homepage.html')
+    return render_template('home.html')
 
-@app.route('/register')
+@app.route('/central')
+def central():
+    return render_template('central.html')
+
+@app.route('/register', methods=['GET', 'POST'])
 def register():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+        cursor.execute("INSERT INTO users (username, password) VALUES (?, ?)", (username, password))
+        conn.commit()
+        conn.close()
+        return redirect(url_for('central'))
     return render_template('register.html')
+
+@app.route('/login', methods=['GET', 'POST'])
+def login():
+    if request.method == 'POST':
+        username = request.form['username']
+        password = request.form['password']
+        conn = sqlite3.connect('database.db')
+        cursor = conn.cursor()
+        cursor.execute("SELECT * FROM users WHERE username = ? AND password = ?", (username, password))
+        user = cursor.fetchone()
+        conn.close()
+
+
+
+    return render_template('login.html')
+
 if __name__ == '__main__':
     app.run(debug=True)
